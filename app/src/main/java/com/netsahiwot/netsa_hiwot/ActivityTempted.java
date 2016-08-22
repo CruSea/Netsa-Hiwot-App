506ea37a57788f86693071cff9abf0b97a2452a5
Sammie <samuelabate2@gmail.com>
2016-08-22 15:38:38 +0300

    Tempted section has been modified to be able to perform crud operations on the databse and two fragment classes have been added. But all of them are inserted for a temporary reason

--- app/src/main/java/com/netsahiwot/netsa_hiwot/ActivityTempted.java
+++ app/src/main/java/com/netsahiwot/netsa_hiwot/ActivityTempted.java
@@ -1,68 +1,133 @@
 package com.netsahiwot.netsa_hiwot;
 
-
 import android.app.ActionBar;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 public class ActivityTempted extends FragmentActivity {
-	
-	
+
+    ViewPager pager;
+    TabHost host;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tempted);
-        
+
         ActionBar bar = getActionBar();
         bar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.header)));
         bar.setTitle("Tempted");
         bar.setDisplayHomeAsUpEnabled(true);
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+            @Override
+            public void onTabSelected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
+
+            }
+
+            @Override
+            public void onTabUnselected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
+
+            }
+
+            @Override
+            public void onTabReselected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
+
+            }
+        };
+
+        // Add 3 tabs, specifying the tab's text and TabListener
+        for (int i = 0; i < 2; i++) {
+            bar.addTab(
+                    bar.newTab()
+                            .setText("Tab " + (i + 1))
+                            .setTabListener(tabListener));
+        }
+
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new VPAdapter(getSupportFragmentManager(), getApplicationContext()));
 
     }
-    
+
     @Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu, menu);
-		return true;
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		switch (item.getItemId()) {
-			
-		case android.R.id.home:
-            // app icon in action bar clicked; go home
-        	this.finish();
-        	overridePendingTransition(R.anim.open_main, R.anim.close_next);
-			return true;
-			
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                this.finish();
+                overridePendingTransition(R.anim.open_main, R.anim.close_next);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 
     // when back button pressed close database and back to previous page
     @Override
     public void onBackPressed() {
-    	// TODO Auto-generated method stub
-    	super.onBackPressed();
-    	finish();
-    	overridePendingTransition(R.anim.open_main, R.anim.close_next);
+        // TODO Auto-generated method stub
+        super.onBackPressed();
+        finish();
+        overridePendingTransition(R.anim.open_main, R.anim.close_next);
     }
-    
+
     @Override
-	public void onConfigurationChanged(final Configuration newConfig)
-	{
-		// Ignore orientation change to keep activity from restarting
-		super.onConfigurationChanged(newConfig);
-	}
+    public void onConfigurationChanged(final Configuration newConfig) {
+        // Ignore orientation change to keep activity from restarting
+        super.onConfigurationChanged(newConfig);
+    }
+
+    private class VPAdapter extends FragmentPagerAdapter {
+        private String fragments[] = {"CRUD_Frag", ""};
+
+        public VPAdapter(FragmentManager supportFragmentManager, Context applicationContext) {
+            super(supportFragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int postion) {
+            switch (postion) {
+                case 0:
+                    return new CRUD_Frag();
+                case 1:
+                    return new Temp_Frag();
+                default:
+                    return new Temp_Frag();
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragments[position];
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.length;
+        }
+    }
 }
---
+++ app/src/main/java/com/netsahiwot/netsa_hiwot/CRUD_Frag.java
@@ -0,0 +1,19 @@
+package com.netsahiwot.netsa_hiwot;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Sammie on 8/20/2016.
+ */
+public class CRUD_Frag extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.crud_frag, container, false);
+    }
+}
